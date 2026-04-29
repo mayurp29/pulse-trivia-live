a5607ec35c1786c8785d71ecaabf21759fb95c2d
mayurp29 <mayur@aahoa.com>
2026-04-29 12:22:59 -0400

    Fix typo in Supabase key

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const APP_CONFIG = {
   supabaseUrl: "https://ikfcelpstuqgtmqwucwk.supabase.co",
-  supabaseAnonKey: "ssb_publishable_3EtwZj5WhiAuSf0cUfIDyQ_nWtklN8k",
+  supabaseAnonKey: "sb_publishable_3EtwZj5WhiAuSf0cUfIDyQ_nWtklN8k",
 };
